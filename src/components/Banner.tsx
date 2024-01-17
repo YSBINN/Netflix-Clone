5334b20387bf15f51a7b3f711abeba82e780d9c1
YSB <dowkdg@naver.com>
2024-01-17 10:32:20 +0900

    Feat : Banner 기능 추가 및 Movies Type 추가

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,152 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "../api/baseApi";
+import requests from "../api/requests";
+import tw from "twin.macro";
+import { MovieDetails } from "../../types/movies";
 
 export default function Banner() {
-  return <div>Banner</div>;
+  const [movie, setMovie] = useState<MovieDetails>();
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetchDate();
+  }, []);
+
+  const truncate = (str: string | undefined, n: number): string => {
+    if (str === undefined) return "";
+    return str.length > n ? str.slice(0, n - 1) + "..." : str;
+  };
+
+  const fetchDate = async () => {
+    const request = await axios.get(requests.fetchNowPlaying);
+
+    const movieId =
+      request.data.results[
+        Math.floor(Math.random() * request.data.results.length)
+      ].id;
+
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: "videos" },
+    });
+    setMovie(movieDetail);
+  };
+
+  if (!movie) return <></>;
+
+  if (!isClicked) {
+    return (
+      <BannerHeader>
+        <BannerImg
+          src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+          alt={movie.title || movie.original_title}
+        />
+        <BannerContents>
+          <BannerTitle>{movie.title || movie.original_title}</BannerTitle>
+          <BannerButtons>
+            <BannerPlayButton onClick={() => setIsClicked(true)}>
+              Play
+            </BannerPlayButton>
+            <BannerInfoButton>More Information</BannerInfoButton>
+          </BannerButtons>
+          <BannerDesc>{truncate(movie.overview, 200)}</BannerDesc>
+        </BannerContents>
+        <BannerFadeBotton />
+      </BannerHeader>
+    );
+  } else {
+    return (
+      <Container>
+        <HomeContainer>
+          <Ifram
+            width="640"
+            height="360"
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            title="YouTube video player"
+            allow="autoplay; fullscreen"
+          ></Ifram>
+        </HomeContainer>
+      </Container>
+    );
+  }
 }
+
+////Banner styles///////
+const BannerHeader = tw.header`relative`;
+
+const BannerImg = tw.img`
+object-cover 
+h-[448px]
+w-full`;
+
+const BannerContents = tw.div`
+ml-[40px]
+pt-[140px]
+h-[190px]
+absolute top-0 left-0`;
+
+const BannerTitle = tw.h1`
+text-5xl
+font-extrabold
+pb-2
+text-gray-600`;
+
+const BannerButtons = tw.div`
+flex
+flex-row
+mt-[15px]`;
+
+const BannerPlayButton = tw.button`
+bg-white 
+text-black 
+flex 
+flex-row 
+justify-start 
+items-center 
+text-xs 
+font-bold 
+rounded-[0.2vw] 
+p-[0.4rem 1.8rem 0.4rem 1rem] 
+mr-4
+hover:(text-white bg-black transition-all delay-200)`;
+
+const BannerInfoButton = tw(BannerPlayButton)`bg-[rgba(109,109,110,0.7)] 
+text-white
+hover:(bg-[rgb(74,74,84)] text-black)`;
+
+const BannerDesc = tw.h1`
+w-[45rem]
+mt-[15px]
+text-gray-600
+leading-tight
+font-medium
+text-base
+max-w-[400px]
+h-[80px]`;
+const BannerFadeBotton = tw.div`
+h-[7.4rem]
+bg-gradient-to-t from-transparent via-[rgba(37,37,37,0.61)] to-[#111]`;
+
+////Banner Detail styles///////
+
+const Container = tw.div`
+  flex 
+  justify-center 
+  items-center
+  flex-col
+  w-full
+  h-screen
+`;
+
+const HomeContainer = tw.div`
+w-full
+h-full
+`;
+
+const Ifram = tw.iframe`
+w-full
+h-full
+z-[1]
+opacity-65
+border-none
+after:(absolute top-0 left-0 w-full h-full)
+`;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -26,18 +26,29 @@ export default function Nav() {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Netflix_2014_logo.svg/300px-Netflix_2014_logo.svg.png?20140810085105"
           onClick={() => window.location.reload()}
         />
-        <UserLogo
-          alt="User ogo"
-          src="https://images.ctfassets.net/y2ske730sjqp/6HVwxZ6sJIwTBu1y11pkPX/883966c47f587ef2fc0ddb419b37959f/BrandAssets_Logos_ApplicationExamples_Carousel-06.jpg"
-        />
       </NetflixNav>
     </>
   );
 }
 
 const NetflixNav = styled.nav<{ $show: boolean }>(
-  tw`fixed top-0 w-full h-[30px] z-1 p-20 flex justify-between items-center transition-all duration-500`,
+  tw`
+  fixed
+  top-0
+  w-full
+  h-[30px]
+  z-1
+  p-20
+  flex
+  justify-between
+  items-center
+  transition-all duration-500
+  cursor-pointer`,
   props => props.$show && tw`bg-black`
 );
-const NetflixLogo = tw.img`fixed left-40 w-[200px] h-[50px] object-fill`;
-const UserLogo = tw.img`fixed right-40 w-[80px] h-[80px] object-cover rounded-full`;
+const NetflixLogo = tw.img`
+fixed
+left-40
+w-[200px]
+h-[50px]
+object-fill`;
